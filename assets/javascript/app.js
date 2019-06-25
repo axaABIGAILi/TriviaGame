2cbcbbcbffb8fbd4c551936c1d4e53f057b8143e
Abi D <axa.abigail.i@gmail.com>
2019-06-25 18:39:42 -0500

    made function makeQuiz to populate quiz div and refined so it dynamically adds questions with loops. stylized question headers.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,74 @@
-// create an object with quiz questions
+//document.ready
+$(document).ready(function () { 
+
+// create an object with quiz questions & answers
 var quizQuestions = [{
-    q1: {
-        question: "Who is the painter responsible for the famous Mona Lisa?";
-        answers: {
-            a: "Leonardo Di Caprio";
-            b: "Vincent Van Gogh";
-            c: "Michaelangelo";
-            d: "Leondardo Da Vinci";
-        }
-        correct: "d";
+        q: 'Who is the painter responsible for the famous Mona Lisa?',
+        answers: [
+            'Leonardo Di Caprio',
+            'Vincent Van Gogh',
+            'Michaelangelo',
+            'Leondardo Da Vinci',
+        ],
+        correct: 'd'
     },
-    q2: {
-        question: "Salvador Dali is famous for ushering in what artistic movement?";
-        answers: {
-            a: "Realism";
-            b: "Surrealism";
-            c: "Cubism";
-            d: "Vandalism";
-        }
-        correct: "b";
+    {   q: 'Salvador Dali is famous for ushering in what artistic movement?',
+        answers: [
+            'Realism',
+            'Surrealism',
+            'Cubism',
+            'Vandalism',
+        ],
+        correct: 'b'
     },
-    q3: {
-        question: "Which artist is famous for the self-portrait of himself in which he has a bandaged ear?";
-        answers: {
-            a: "Vincent Van Gogh";
-            b: "Pablo Picasso";
-            c: "John Lennon";
-            d: "Georgia O' Keefe";
-        }
-        correct: "a";
+    {   q: 'Which artist is famous for the self-portrait of himself in which he has a bandaged ear?',
+        answers: [
+            'Vincent Van Gogh',
+            'Pablo Picasso',
+            'John Lennon',
+            'Claude Monet',
+        ],
+        correct: 'a'
     },
-    q4: {
-        question: "Andy Warhol is known for his use of...";
-        answers: {
-            a: "Vivid, high contrast colors";
-            b: "Subject matters from pop culture";
-            c: "Repitition and patterns";
-            d: "All of the above";
-        }
-        correct: "d";
+    {   q: 'Andy Warhol is known for his use of...',
+        answers: [
+            'Vivid, high contrast colors',
+            'Subject matters from pop culture',
+            'Repitition and patterns',
+            'All of the above',
+        ],
+        correct: 'd'
 
     },
-    q5: {
-        question: "True or false: MC Escher was a dutch rapper who dropped sick beats in the clubs before discovering his love for mathematically inspired art."
-        answers: {
-            a: "True";
-            b: "False";
-        }
-        correct: "b";
+    {   q: 'True or false: MC Escher was a dutch rapper who dropped sick beats in the clubs before discovering his love for mathematically inspired art.',
+        answers: [
+            'True',
+            'False',
+        ],
+        correct: 'b'
     }
-}];
+];
 
-// create a function to dynamically populate "quiz" div with quiz questions (randomly?)
+// console log to test array (make sure it's coded properly)
+console.log(quizQuestions[1]);
+console.log(quizQuestions[0].answers[0]);
+console.log(quizQuestions[2].answers[1]);
+
+// create a function to dynamically populate 'quiz' div with quiz questions (randomly?)
 function makeQuiz () {
-
     // loop through the quizQuestions object to populate the questions DIV
-    for (i=0; i < quizQuestions.length; i++) {
-        $("#questions").
+    for (var i=0; i < quizQuestions.length; i++) {
+        $('#questions').append('<div class="question" id="q' + i +'"></div>');
+        $('#q'+i).append('<h3>' + quizQuestions[i].q + '</h3>');
+        // loop to append each individual answer
+        for (var j=0; j < quizQuestions[i].answers.length ; j++) {
+        $('#q'+i).append('<input type="radio" value="' + quizQuestions[i].answers[j] + '" name=" ' + quizQuestions[i].answers[i] + ' " />' + '<label>' + quizQuestions[i].answers[j] + '</label>' + ' ');
+        }
 
     }
 }
 
-// code
+makeQuiz();
+
+// close doc.ready
+});
